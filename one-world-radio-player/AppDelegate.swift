1277e7a3954755bae2ed51d36bde4a75039095a6
hirossan4049 <haruto405329@gmail.com>
2020-12-17 20:36:47 +0900

    Initial Commit

---
+++ one-world-radio-player/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  one-world-radio-player
+//
+//  Created by unkonow on 2020/12/17.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ one-world-radio-player/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  one-world-radio-player
+//
+//  Created by unkonow on 2020/12/17.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
